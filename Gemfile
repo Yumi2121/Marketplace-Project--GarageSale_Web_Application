bd649f0c3062a5064e1a83d7fc5b91a3fc2d858b
Yumi2121 <zumin2016@gmail.com>
2022-06-20 16:46:52 +1000

    start deployment

--- Gemfile
+++ Gemfile
@@ -59,3 +59,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 
 gem "aws-sdk-s3", require: false
+
+gem "aws-sdk-locationservice", "~> 1.22"
+
+gem "geocoder", "~> 1.8"
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   enum category: {Clothes: 0, Toys: 1, Kitchenware: 2, Furniture: 3, Misc: 4}
   has_one_attached :item_image
+  # if don't have has_one_attached, will show -- unknown attribute 'image' for Item.
 end
