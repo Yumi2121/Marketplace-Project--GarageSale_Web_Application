bb631fc7d0ffbe8e28d772c2660cce92f4627907
Yumi2121 <zumin2016@gmail.com>
2022-06-21 16:59:04 +1000

    geocde TestGeo issue

--- app/experiment/test_geo.rb
+++ app/experiment/test_geo.rb
@@ -1,4 +1,4 @@
-require 'geocoder'
-
-results = Geocoder.search("1 Highland ave, Mitcham, 3132")
-puts results.first.coordinates
+# require 'geocoder'
+#
+# results = Geocoder.search("1 Highland ave, Mitcham, 3132")
+# puts results.first.coordinates
