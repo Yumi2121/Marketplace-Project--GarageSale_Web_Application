8d2a6b81064ecd596ac42894df57585687958981
Yumi2121 <zumin2016@gmail.com>
2023-01-10 16:46:01 +1100

    delete mailgun

--- app/helpers/mailgun_helper.rb
+++ app/helpers/mailgun_helper.rb
@@ -1,20 +1,20 @@
-require 'mailgun-ruby'
+# require 'mailgun-ruby'
 
-module MailgunHelper
+# module MailgunHelper
 
-def send_email(from, to, subject, text)
-  api_key = Rails.application.credentials.mailgun[:api_key]
-  domain = Rails.application.credentials.mailgun[:domain]
-  mg_client = Mailgun::Client.new(api_key)
+# def send_email(from, to, subject, text)
+#   api_key = Rails.application.credentials.mailgun[:api_key]
+#   domain = Rails.application.credentials.mailgun[:domain]
+#   mg_client = Mailgun::Client.new(api_key)
 
-  message_params =  {
-    from: from,
-    to:   to,
-    subject: subject,
-    text:    text
-  }
+#   message_params =  {
+#     from: from,
+#     to:   to,
+#     subject: subject,
+#     text:    text
+#   }
 
-  mg_client.send_message(domain, message_params).to_h!
-end
+#   mg_client.send_message(domain, message_params).to_h!
+# end
 
-end
+# end
